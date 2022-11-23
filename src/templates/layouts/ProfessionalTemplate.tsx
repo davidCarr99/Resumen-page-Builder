e5715f08eaf5fd68b5f803d97c95ca906256df45
LeoKrr <davidcarr1215@gmail.com>
2022-11-23 14:39:22 -0500

    creacion de nueva vistas y secciones

--- src/templates/layouts/ProfessionalTemplate.tsx
+++ src/templates/layouts/ProfessionalTemplate.tsx
@@ -25,7 +25,7 @@ import {
 } from 'src/stores/data.store';
 
 const ResumeContainer = styled(Flex)`
-  height: 100%;
+  height: 120%;
   padding: 40px 25px;
   column-gap: 10px;
   color: ${(props) => props.theme.fontColor};
